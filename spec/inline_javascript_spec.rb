1b33a814a4430a7fb9aeb8ac2b724d03b334ec64
Stephen Hardisty <stephen.hardisty@rea-group.com>
2011-02-25 13:39:35 +1100

    added tests for more complex types

--- spec/inline_javascript_spec.rb
+++ spec/inline_javascript_spec.rb
@@ -5,4 +5,14 @@ describe Inline::Javascript do
     js.concat('bath', 'rules').should == 'bath rules'
   end
 
+  it "should be able to deal with numeric values" do
+    js = Inline::Javascript.new('function add (a, b) { return(a + b) }')
+    js.add(1, 100).should == 101
+  end
+
+  it "should handle more complex types" do
+    js = Inline::Javascript.new('function something (some_array) { return {second: some_array[1]} }')
+    js.something(%w{hopefully this works great}).should == {'second' => 'this'}
+  end
+
 end
