eafdf5d4d7ff090e4cf70c9d9c5f299cd97e3c69
Stephen Hardisty <moowahaha@hotmail.com>
2011-02-27 16:57:11 +1100

    added documentation

--- lib/inline_java_script.rb
+++ lib/inline_java_script.rb
@@ -1,7 +1,24 @@
 require 'json'
 
+#== SYNOPSIS:
+#
+#To call a JavaScript function as if it was Ruby:
+#
+#    js = InlineJavaScript.new('function my_add (a, b) { return a+b}')
+#    js.my_add(1, 2) #=> 3
+#
+#Using more complex types:
+#
+#    js = InlineJavaScript.new('function my_concat (a) { return {concat_string: a[0] + " " + a[1]} }')
+#    js.my_concat(['hello', 'world']) #=> {"concat_string" => "hello world"}
+
+
 class InlineJavaScript
-  VERSION = '0.0.1'
+  VERSION = '0.1'
+
+  # Takes a string containing some JavaScript and executes it. The state is maintained.
+  # JavaScript functions defined in the string can be called directly from an instance
+  # of InlineJavaScript as if they were methods.
 
   def initialize javascript_functions
     require File.join(File.dirname(__FILE__), %w{.. ext inline_java_script_v8_wrapper inline_java_script_v8_wrapper})
@@ -9,6 +26,10 @@ class InlineJavaScript
     @wrapper = InlineJavaScript::V8Wrapper.new
     @wrapper.execute(javascript_functions)
   end
+
+  # Attempts to call whatever JavaScript function you defined in the constructor.
+  # It will accept any number of parameters of String, Fixnum, Array or Hash and can
+  # support returning the same from JavaScript.
 
   def method_missing(method, *arguments, &unused_block)
     json_arguments = arguments.map do |arg|
--- lib/inline_java_script/v8_wrapper.rb
+++ lib/inline_java_script/v8_wrapper.rb
@@ -1,1 +1,19 @@
 require File.join(File.dirname(__FILE__), %w{.. .. ext inline_java_script_v8_wrapper inline_java_script_v8_wrapper})
+
+#== SYNOPSIS:
+#To call the V8 engine:
+#
+#    wrapper = InlineJavaScript::V8Wrapper.new
+#    wrapper.execute('1 + 2') #=> "3"
+#
+#An instance of the V8Wrapper maintains state:
+#
+#    wrapper = InlineJavaScript::V8Wrapper.new
+#    wrapper.execute('var e = "monster"')
+#    wrapper.execute('e') #=> "monster"
+#
+#    # ... But I cannot access another instance...
+#    InlineJavaScript::V8Wrapper.new.execute('e') #=> SyntaxError
+
+class InlineJavaScript::V8Wrapper
+end
